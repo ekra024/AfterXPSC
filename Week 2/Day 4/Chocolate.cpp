28098998c1dd04be12dabf4fc9a2f0addbc9ddaa
Asmahul Husna <ekra.cse8.bu@gmail.com>
2023-11-30 20:57:13 +0600

    week 02

---
+++ Week 2/Day 4/Chocolate.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b) a/(b/__gcd(a,b))
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using pbds = tree<T, null_type, less<T>,rb_tree_tag, tree_order_statistics_node_update>;
+int mod = 1e9 + 7;
+const int N = 1e7 + 123;
+bitset<N> is_prime;
+vector<int>prime;
+int ans[N];
+
+int main()
+{
+    optimize();
+    ll n; cin >> n;
+    vector<ll> a(n);
+    ll chocolate = 1, cnt = 0;
+    bool one = false;
+
+    for(ll i = 0; i < n; i++) cin >> a[i];
+
+    for(ll i = 0; i < n; i++) {
+        if(a[i] == 1 && one) {
+            chocolate *= (cnt+1);
+            cnt = 0;
+        }
+        else if(a[i] == 1) {
+            one = true;
+        }
+        else if(a[i] == 0 && one) cnt++;
+    }
+
+    if(one == false) cout << 0 << '\n';
+    else
+    cout << chocolate << '\n';
+}
+
+
